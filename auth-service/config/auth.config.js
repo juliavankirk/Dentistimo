fece67ba7777cbc08830d8f441f50daad1ccc046
istrate <istrate@net.chalmers.se>
2022-12-07 09:16:54 +0100

    remove old auth backend

--- auth-service/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret: "team12-is-the-best"
-}
--- auth-service/models/user-roles.model.js
+++
@@ -1,6 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const UserRole = sequelize.define('user_role', {
-        selfGranted: Sequelize.BOOLEAN
-    }, { timestamps: false });
-    return UserRole;
-};
